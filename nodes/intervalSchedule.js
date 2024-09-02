46debeb513c4618a429c069ad2364de4750239d0
lztpro <11836068+lztpro@user.noreply.gitee.com>
2024-09-02 11:36:20 +0800

    修复了addschedule重名操作导致中断

--- nodes/intervalSchedule.js
+++ nodes/intervalSchedule.js
@@ -1,9 +1,9 @@
 module.exports = function(RED) {
   function IntervalSchedule(config) {
-    RED.nodes.createNode(this,config);
-    const node = this;
-    node.name = config.name;
-    let timers = {};
+    RED.nodes.createNode(this,config)
+    const node = this
+    node.name = config.name
+    let timers = {}
     
     node.on('input', function(msg, send, done) {
       // NodeRed 兼容性代码
@@ -12,15 +12,15 @@ module.exports = function(RED) {
       try {
         if (typeof msg.payload === 'object' && msg.payload !== null) {
           if (msg.payload.command === 'add') {
-            addInterval(msg.payload);
+            addInterval(msg.payload)
           } else if (msg.payload.command === 'remove') {
-            removeInterval(msg.payload);
+            removeInterval(msg.payload)
           }
         } else {
-        done(new Error("Payload is not a valid JSON object"));
+        done(new Error("Payload is not a valid JSON object"))
         }
       } catch (error) {
-        console.debug(error);
+        console.debug(error)
       }
       
     })
@@ -30,8 +30,12 @@ module.exports = function(RED) {
       // 左——>右:新msg内的payload,传入的payload以及传入的payload中的'payload'
       const msg = {payload:payload.payload}
       if (!timers[payload.name]) {
-        timers[payload.name] = setInterval(() => { node.send(msg);
-        }, payload.intervalTime * 1000);
+        timers[payload.name] = {
+          interval: setInterval(() => { 
+            node.send(msg)
+          }, payload.intervalTime * 1000),
+          msg: msg
+        }
       } else {
         updateInterval(payload)
       }
@@ -39,18 +43,18 @@ module.exports = function(RED) {
     }
 
     function removeInterval(payload) {
-      clearInterval(timers[payload.name]);
+      clearInterval(timers[payload.name])
     }
 
     function updateInterval(payload) {
-      clearInterval(timers[payload.name]);
-      timers[payload.name] = setInterval(() => {
-        node.send(msg);
-      }, payload.intervalTime * 1000);
+      clearInterval(timers[payload.name].interval)
+      timers[payload.name].interval = setInterval(() => {
+        node.send(timers[payload.name].msg)
+      }, payload.intervalTime * 1000)
     }
 
   }
 
-  RED.nodes.registerType("intervalSchedule",IntervalSchedule);
+  RED.nodes.registerType("intervalSchedule",IntervalSchedule)
 
 }
