87c34714f967642381748cd0684a29072dbcddd2
lztpro <11836068+lztpro@user.noreply.gitee.com>
2024-09-12 09:27:53 +0800

    修复了updateIntervalTask不生效的问题

--- nodes/intervalSchedule.js
+++ nodes/intervalSchedule.js
@@ -12,9 +12,9 @@ module.exports = function(RED) {
       try {
         if (typeof msg.payload === 'object' && msg.payload !== null) {
           if (msg.payload.command === 'add') {
-            addInterval(msg.payload)
+            addIntervalTask(msg.payload)
           } else if (msg.payload.command === 'remove') {
-            removeInterval(msg.payload)
+            removeIntervalTask(msg.payload)
           }
         } else {
         done(new Error("Payload is not a valid JSON object"))
@@ -22,40 +22,38 @@ module.exports = function(RED) {
       } catch (error) {
         console.debug(error)
       }
-      
+
     })
       
-
-    function addInterval (payload) {
+    function addIntervalTask (payload) {
       // 左——>右:新msg内的payload,传入的payload以及传入的payload中的'payload'
-      const msg = {payload:payload.payload}
+      const newMsg = {payload:payload.payload}
       if (!timers[payload.name]) {
         timers[payload.name] = {
+          msg: newMsg,
           interval: setInterval(() => { 
-            node.send(msg)
+            node.send(timers[payload.name].msg)
           }, payload.intervalTime * 1000),
-          msg: msg
         }
       } else {
-        updateInterval(payload)
+        updateIntervalTask(payload)
       }
-      
     }
 
-    function removeInterval(payload) {
+    function removeIntervalTask(payload) {
       if (timers[payload.name]) {
         clearInterval(timers[payload.name].interval) // 删除定时器对象中的计时器
         delete timers[payload.name] // 删除计时器对象
       }
     }
 
-    function updateInterval(payload) {
+    function updateIntervalTask(payload) {
+      timers[payload.name].msg.payload = payload.payload // 更新为新定时任务中的payload内容
       clearInterval(timers[payload.name].interval)
       timers[payload.name].interval = setInterval(() => {
         node.send(timers[payload.name].msg)
       }, payload.intervalTime * 1000)
     }
-
   }
 
   RED.nodes.registerType("intervalSchedule",IntervalSchedule)
